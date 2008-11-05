1f72850fbfd1d5462cf93c81c92b930deabd776e
Adrian Kuhn <akuhn@iam.unibe.ch>
2008-11-05 15:05:32 +0000

    Row row row fight the subclipse.

--- test/Example.java
+++ test/Example.java
@@ -19,16 +19,7 @@ public class Example {
 
 	public static void main(String[] args) {
 		System.out.println(
-		
-		        // To execute this code
-		        //
-		        // 1. run numeral.ProcRunner in folder src => compiles this file
-                // 2. run this file => prints "2000"
-		        // 3. ????
-		        // 4. Profit!
-		        
 				MCMLXXVII + XXIV
-		
 		);
 	}
 	
